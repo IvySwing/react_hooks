cd826c7d420ba7e56526001439c15479ad962241
Kori Riddick <entense@att.net>
2020-06-24 22:18:20 -0400

    Updates

---
+++ precache-manifest.3edbd0cf6fa5c7528d76fd90d11dab3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a141bfd1e529b1607e6b7819ca83b583",
+    "url": "/react_hooks/index.html"
+  },
+  {
+    "revision": "9dc00896743092bad2e9",
+    "url": "/react_hooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "644d0e246741b8221f32",
+    "url": "/react_hooks/static/js/2.dcba3a17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hooks/static/js/2.dcba3a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc00896743092bad2e9",
+    "url": "/react_hooks/static/js/main.e5504897.chunk.js"
+  },
+  {
+    "revision": "796104a8aebb7fc015b0",
+    "url": "/react_hooks/static/js/runtime-main.f207f535.js"
+  }
+]);
